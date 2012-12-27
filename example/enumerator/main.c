2667615ee5b5a643c9527afc2f4a69f8b7436561
Liam Staskawicz <liam@sifteo.com>
2012-12-26 17:48:55 -0800

    enumerator example: traverse device descriptors to print out more info

--- example/enumerator/main.c
+++ example/enumerator/main.c
@@ -2,38 +2,9 @@
 #include <stdio.h>
 #include "usbus.h"
 
-static void onDeviceConnected(struct UsbusDevice *d, uint8_t* dispose)
-{
-    *dispose = 1;
-
-    struct UsbusDeviceDescriptor desc;
-    usbusGetDescriptor(d, &desc);
-    printf("connect: vid 0x%x, pid 0x%x\n", desc.idVendor, desc.idProduct);
-
-    if (usbusOpen(d) == UsbusOK) {
-
-        if (usbusClaimInterface(d, 0) == UsbusOK) {
-            char str[256];
-            unsigned len;
-            if (usbusGetStringDescriptorAscii(d, desc.iProduct, 0, str, sizeof str, &len) == UsbusOK) {
-                printf("product: %s\n", str);
-            }
-
-            if (usbusGetStringDescriptorAscii(d, desc.iManufacturer, 0, str, sizeof str, &len) == UsbusOK) {
-                printf("mfgr: %s\n", str);
-            }
-        }
-
-        usbusClose(d);
-    }
-}
-
-static void onDeviceDisconnected(struct UsbusDevice *d)
-{
-    struct UsbusDeviceDescriptor desc;
-    usbusGetDescriptor(d, &desc);
-    printf("disconnect: vid 0x%x, pid 0x%x\n", desc.idVendor, desc.idProduct);
-}
+static void dumpDeviceInfo(UsbusDevice *d, struct UsbusDeviceDescriptor *desc);
+static void onDeviceConnected(struct UsbusDevice *d, uint8_t* dispose);
+static void onDeviceDisconnected(struct UsbusDevice *d);
 
 int main(int argc, char **argv)
 {
@@ -42,4 +13,96 @@ int main(int argc, char **argv)
     usbusStopListen(0);
 
     return 0;
+}
+
+void onDeviceConnected(struct UsbusDevice *d, uint8_t* dispose)
+{
+    *dispose = 1;
+
+    struct UsbusDeviceDescriptor desc;
+    usbusGetDescriptor(d, &desc);
+    printf("connect: vid 0x%x, pid 0x%x\n", desc.idVendor, desc.idProduct);
+
+    if (usbusOpen(d) == UsbusOK) {
+        dumpDeviceInfo(d, &desc);
+        usbusClose(d);
+    }
+}
+
+void onDeviceDisconnected(struct UsbusDevice *d)
+{
+    struct UsbusDeviceDescriptor desc;
+    usbusGetDescriptor(d, &desc);
+    printf("disconnect: vid 0x%x, pid 0x%x\n", desc.idVendor, desc.idProduct);
+}
+
+
+
+void dumpDeviceInfo(UsbusDevice *d, struct UsbusDeviceDescriptor *desc)
+{
+    int r = usbusClaimInterface(d, 0);
+    if (r != UsbusOK) {
+        return;
+    }
+
+    /*
+     * Dump string descriptors
+     */
+
+    char str[256];
+    unsigned len;
+    if (usbusGetStringDescriptorAscii(d, desc->iProduct, 0, str, sizeof str, &len) == UsbusOK) {
+        printf("product: %s\n", str);
+    }
+
+    if (usbusGetStringDescriptorAscii(d, desc->iManufacturer, 0, str, sizeof str, &len) == UsbusOK) {
+        printf("mfgr: %s\n", str);
+    }
+
+    // iterate each device configuration
+    unsigned c;
+    for (c = 0; c < desc->bNumConfigurations; ++c) {
+
+        struct UsbusConfigDescriptor cfgDesc;
+        r = usbusGetConfigDescriptor(d, c, &cfgDesc);
+        if (r != UsbusOK) {
+            printf("cfgdesc fail\n");
+            return;
+        }
+
+
+        printf("config descriptor: bNumInterfaces: %d\n", cfgDesc.bNumInterfaces);
+        printf("config descriptor: iConfiguration: %d\n", cfgDesc.iConfiguration);
+
+        // iterate the interfaces for each configuration
+        unsigned i;
+        for (i = 0; i < cfgDesc.bNumInterfaces; ++i) {
+
+            struct UsbusInterfaceDescriptor intfDesc;
+            r = usbusGetInterfaceDescriptor(d, i, 0, &intfDesc);
+            if (r != UsbusOK) {
+                printf("intf descriptor: fail\n");
+                return;
+            }
+
+            printf("intf descriptor %d: bNumEndpoints %d\n", i, intfDesc.bNumEndpoints);
+            printf("intf descriptor %d: iInterface %d\n", i, intfDesc.iInterface);
+
+            // iterate the endpoints for each interface
+            unsigned e;
+            for (e = 0; e < intfDesc.bNumEndpoints; ++e) {
+
+                struct UsbusEndpointDescriptor epDesc;
+                r = usbusGetEndpointDescriptor(d, intfDesc.iInterface, e, &epDesc);
+                if (r != UsbusOK) {
+                    printf("ep descriptor %d: fail\n", e);
+                    return;
+                }
+
+                printf("ep descriptor %d: bEndpointAddress 0x%02x, bInterval %d, wMaxPacketSize %d\n",
+                       e, epDesc.bEndpointAddress, epDesc.bInterval, epDesc.wMaxPacketSize);
+            }
+        }
+    }
+
 }
